08bbda3cc83c049ee59155bb58df27bbc67f3ee7
ben-toogood <bentoogood@gmail.com>
2020-02-19 17:20:42 +0000

    Implement GitHub Actions Webhooks (#52)
    * Implement GitHub Webhooks
    
    * Refactor

--- web/handler/github/github.go
+++ web/handler/github/github.go
@@ -1,14 +1,16 @@
 package github
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
 
 	"github.com/micro/go-micro/v2/web"
 	platform "github.com/micro/platform/service/proto"
-	"github.com/micro/platform/web/util"
+	utils "github.com/micro/platform/web/util"
 )
 
 // Handler encapsulates the events handlers
@@ -23,7 +25,55 @@ func RegisterHandlers(srv web.Service) error {
 	}
 
 	srv.HandleFunc("/v1/github/webhook", h.WebhookHandler)
+	srv.HandleFunc("/v1/github/build-started", h.BuildStartedHandler)
+	srv.HandleFunc("/v1/github/build-finished", h.BuildFinishedHandler)
 	return nil
+}
+
+// BuildStartedHandler process the github webhook for a docker build starting
+func (h *Handler) BuildStartedHandler(w http.ResponseWriter, req *http.Request) {
+	// Extract the request body containing the webhook data
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		utils.Write500(w, err)
+		return
+	}
+
+	// Unmarshal the bytes into a struct
+	var data []string
+	if err := json.Unmarshal(body, &data); err != nil {
+		utils.Write500(w, err)
+		return
+	}
+
+	// Create the events
+	err = h.createEventsForFiles(req.Context(), platform.EventType_BuildStarted, data)
+	if err != nil {
+		utils.Write500(w, err)
+	}
+}
+
+// BuildFinishedHandler process the github webhook for a docker build starting
+func (h *Handler) BuildFinishedHandler(w http.ResponseWriter, req *http.Request) {
+	// Extract the request body containing the webhook data
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		utils.Write500(w, err)
+		return
+	}
+
+	// Unmarshal the bytes into a struct
+	var data []string
+	if err := json.Unmarshal(body, &data); err != nil {
+		utils.Write500(w, err)
+		return
+	}
+
+	// Create the events
+	err = h.createEventsForFiles(req.Context(), platform.EventType_BuildFinished, data)
+	if err != nil {
+		utils.Write500(w, err)
+	}
 }
 
 // WebhookHandler processes the GitHub push webhooks
@@ -45,17 +95,30 @@ func (h *Handler) WebhookHandler(w http.ResponseWriter, req *http.Request) {
 	}
 
 	// Get the directories (services) which have been impacted
-	srvs := []string{}
+	files := []string{}
 	for _, c := range data.Commits {
-		srvs = append(srvs, c.ServicesImpacted()...)
+		files = append(files, c.Files()...)
 	}
-	srvs = uniqueStrings(srvs)
 
-	// Create push events for the servies
-	for _, srv := range srvs {
-		_, err := h.platform.CreateEvent(req.Context(), &platform.CreateEventRequest{
+	// create the events
+	err = h.createEventsForFiles(req.Context(), platform.EventType_SourceUpdated, files)
+	if err != nil {
+		utils.Write500(w, err)
+	}
+}
+
+func (h *Handler) createEventsForFiles(ctx context.Context, event platform.EventType, files []string) error {
+	srvs := []string{}
+	for _, f := range files {
+		if c := strings.Split(f, "/"); len(c) > 1 {
+			srvs = append(srvs, c[0])
+		}
+	}
+
+	for _, srv := range uniqueStrings(srvs) {
+		_, err := h.platform.CreateEvent(ctx, &platform.CreateEventRequest{
 			Event: &platform.Event{
-				Type: platform.EventType_SourceUpdated,
+				Type: event,
 				Service: &platform.Service{
 					Name: srv,
 				},
@@ -63,9 +126,12 @@ func (h *Handler) WebhookHandler(w http.ResponseWriter, req *http.Request) {
 		})
 
 		if err != nil {
-			utils.Write500(w, err)
+			fmt.Println(srv, event)
+			return err
 		}
 	}
+
+	return nil
 }
 
 type commit struct {
@@ -74,19 +140,9 @@ type commit struct {
 	Modified []string
 }
 
-func (c *commit) ServicesImpacted() []string {
-	allFiles := []string{}
-	allFiles = append(c.Added, c.Removed...)
-	allFiles = append(allFiles, c.Modified...)
-
-	dirs := []string{}
-	for _, f := range allFiles {
-		if c := strings.Split(f, "/"); len(c) > 1 {
-			dirs = append(dirs, c[0])
-		}
-	}
-
-	return uniqueStrings(dirs)
+func (c *commit) Files() []string {
+	files := append(c.Added, c.Removed...)
+	return append(files, c.Modified...)
 }
 
 func uniqueStrings(input []string) []string {
