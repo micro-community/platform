df053d4dce87fd0481193db474f7beac80b2c533
Asim Aslam <asim@aslam.me>
2020-02-22 00:40:36 +0000

    fix the proto values (#73)

--- web/app/src/app/events-list/events-list.component.ts
+++ web/app/src/app/events-list/events-list.component.ts
@@ -2,21 +2,23 @@ import { Component, OnInit, Input } from "@angular/core";
 import * as types from "../types";
 
 const eventTypes = {
-  1: "DepoymentCreated",
-  2: "DepoymentDeleted",
-  3: "DepoymentUpdated",
+  1: "ServiceCreated",
+  2: "ServiceDeleted",
+  3: "ServiceUpdated",
   4: "SourceUpdated",
   5: "BuildStarted",
   6: "BuildFinished"
+  7: "BuildFailed"
 };
 
 const eventTypesNice = {
-  1: "deployment created",
-  2: "deployment deleted",
-  3: "deployment updated",
+  1: "service created",
+  2: "service deleted",
+  3: "service updated",
   4: "source updated",
   5: "build started",
   6: "build finished"
+  7: "build failed"
 };
 
 @Component({
