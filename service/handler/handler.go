fb6da424f4c63a8ed958098254f64f194ce13924
ben-toogood <bentoogood@gmail.com>
2020-02-24 14:52:06 +0000

    Update platform service to use runtime events (#88)
    Co-authored-by: Asim Aslam <asim@aslam.me>
    Co-authored-by: Jake Sanders <i@am.so-aweso.me>

--- service/handler/handler.go
+++ service/handler/handler.go
@@ -4,16 +4,17 @@ import (
 	"context"
 
 	"github.com/micro/go-micro/v2"
+	log "github.com/micro/go-micro/v2/logger"
 	"github.com/micro/go-micro/v2/runtime"
+	runtimepb "github.com/micro/go-micro/v2/runtime/service/proto"
 	"github.com/micro/go-micro/v2/server"
 	"github.com/micro/go-micro/v2/store"
-	log "github.com/micro/go-micro/v2/logger"
 
 	pb "github.com/micro/platform/service/proto"
 )
 
 // Topic aysnc messages are published to
-var Topic = "go.micro.platform.events"
+var Topic = "go.micro.runtime.events"
 
 // Handler implements the platform service interface
 type Handler struct {
@@ -46,7 +47,21 @@ func NewHandler(srv micro.Service) *Handler {
 // HandleEvent such as service created, updated or deleted. It reformats
 // the request to match the proto and then passes it off to the handler to process
 // as it would any other request, ensuring there is no duplicate logic.
-func (h *Handler) HandleEvent(ctx context.Context, event *pb.Event) error {
-	req := &pb.CreateEventRequest{Event: event}
+func (h *Handler) HandleEvent(ctx context.Context, event *runtimepb.Event) error {
+	req := &pb.CreateEventRequest{Event: &pb.Event{
+		Type:      runtimeEventsMap[event.Type],
+		Timestamp: event.Timestamp,
+		Service: &pb.Service{
+			Name:    event.Service,
+			Version: event.Version,
+		},
+	}}
+
 	return h.CreateEvent(ctx, req, &pb.CreateEventResponse{})
+}
+
+var runtimeEventsMap = map[string]pb.EventType{
+	"create": pb.EventType_ServiceCreated,
+	"update": pb.EventType_ServiceUpdated,
+	"delete": pb.EventType_ServiceDeleted,
 }
