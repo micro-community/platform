52e248f93e23c6bf3c6c5bb30c492635b105a945
ben-toogood <bentoogood@gmail.com>
2020-03-02 09:59:15 +0000

    Platform Event IDs (#118)
    Co-authored-by: Janos Dobronszki <dobronszki@gmail.com>

--- service/go.mod
+++ service/go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/golang/protobuf v1.3.3
+	github.com/google/uuid v1.1.1
 	github.com/kr/pretty v0.2.0 // indirect
 	github.com/micro/go-micro/v2 v2.1.3-0.20200227161105-e21ed3a183ea
 	golang.org/x/sys v0.0.0-20200124204421-9fbb57f87de9 // indirect
--- service/handler/events.go
+++ service/handler/events.go
@@ -5,8 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"sort"
-	"strings"
 	"time"
+
+	"github.com/google/uuid"
 
 	"github.com/micro/go-micro/v2/errors"
 	"github.com/micro/go-micro/v2/store"
@@ -15,33 +16,58 @@ import (
 
 // ListEvents returns recent events, if a resource is provided then this is scoped to their events
 func (h *Handler) ListEvents(ctx context.Context, req *pb.ListEventsRequest, rsp *pb.ListEventsResponse) error {
-	records, err := h.Store.List()
+	var records []*store.Record
+	var err error
+
+	// Use a prefix to scope to the resource (if one was provided)
+	if req.Service != nil && len(req.Service.Name) > 0 {
+		records, err = h.Store.Read(req.Service.Name, store.ReadPrefix())
+	} else {
+		records, err = h.Store.List()
+	}
+
+	//  Checck for the error
 	if err != nil {
 		return errors.InternalServerError("go.micro.platform", "unable to read from store: %v", err)
 	}
 
-	var prefix string
-
-	// Use a prefix to scope to the resource (if one was provided)
-	if req.Service != nil && len(req.Service.Name) > 0 {
-		prefix = req.Service.Name + ":"
-	}
-
-	// Filter and decode the records
-	for _, r := range records {
-		if !strings.HasPrefix(r.Key, prefix) {
-			continue
-		}
-
-		var e *pb.Event
-		if err := json.Unmarshal(r.Value, &e); err != nil {
+	// Decode the records
+	rsp.Events = make([]*pb.Event, len(records))
+	for i, r := range records {
+		if err := json.Unmarshal(r.Value, &rsp.Events[i]); err != nil {
 			return errors.InternalServerError("go.micro.platform", "unable to decode records")
 		}
-		rsp.Events = append(rsp.Events, e)
 	}
 
 	// sort the events
 	sort.Slice(rsp.Events, func(i, j int) bool { return rsp.Events[i].Timestamp > rsp.Events[j].Timestamp })
+
+	return nil
+}
+
+// GetEvent finds an event by the ID
+func (h *Handler) GetEvent(ctx context.Context, req *pb.GetEventRequest, rsp *pb.GetEventResponse) error {
+	// Perform the validations
+	if req.Event == nil {
+		return errors.BadRequest("go.micro.platform", "missing event")
+	}
+	if len(req.Event.Id) == 0 {
+		return errors.BadRequest("go.micro.platform", "missing event ID")
+	}
+
+	// Find the record
+	records, err := h.Store.Read(req.Event.Id, store.ReadSuffix())
+	if err != nil {
+		return errors.InternalServerError("go.micro.platform", "unable to read from store")
+	}
+	if len(records) == 0 {
+		return errors.NotFound("go.micro.platform", "event not found")
+	}
+
+	// Decode the event
+	if err := json.Unmarshal(records[0].Value, &rsp.Event); err != nil {
+		return errors.InternalServerError("go.micro.platform", "unable to decode record")
+	}
 
 	return nil
 }
@@ -59,11 +85,14 @@ func (h *Handler) CreateEvent(ctx context.Context, req *pb.CreateEventRequest, r
 		return errors.BadRequest("go.micro.platform", "invalid event service")
 	}
 
-	if req.Event.Timestamp == 0 {
-		req.Event.Timestamp = time.Now().Unix()
-	}
 	// Construct the event
 	event := &Event{req.Event}
+	if len(event.Id) == 0 {
+		event.Id = uuid.New().String()
+	}
+	if event.Timestamp == 0 {
+		event.Timestamp = time.Now().Unix()
+	}
 
 	// Write to the store
 	err := h.Store.Write(&store.Record{
@@ -84,7 +113,7 @@ type Event struct {
 
 // Key to be used in the store
 func (e *Event) Key() string {
-	return fmt.Sprintf("%v:%v:%v", e.Service.Name, e.Event.Type.String(), e.Timestamp)
+	return fmt.Sprintf("%v:%v:%v", e.Service.Name, e.Event.Type.String(), e.Id)
 }
 
 // Bytes is the JSON encoded event
